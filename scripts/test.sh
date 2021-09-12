5538dea8729ff7e5777dc221733b4b02835b9c4f
Grace Luo (Berkeley) <graceluo@berkeley.edu>
2021-09-12 15:00:05 -0700

    Update configs

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 MMF_USER_DIR=$PWD \
-  nohup mmf_predict config=${resource_path}/${experiment}/config.yaml \
+  nohup mmf_predict config=configs/private/clip.yaml \
   model=clip \
   dataset=news_clippings \
   run_type=test \
-  checkpoint.resume_file=${resource_path}/${experiment}/test.ckpt \
+  checkpoint.resume_file=runs/${split}_${model}/best.ckpt \
   checkpoint.resume_pretrained=False \
   env.save_dir=runs/${split}_${model} \
   > ${split}_${model}_test.out &
